7374eed7d6312e6a2573e7ca0f7ac195863e8656
Stanislav <stanislav.vozikov@gmail.com>
2019-10-31 22:59:37 +0300

    update

--- helpers/nameOfCategory.js
+++ helpers/nameOfCategory.js
@@ -1,11 +1,14 @@
 exports.kumite = (sexOfCategory, ageGroup, weightCategoty, maxAgeCategory) => {
     let sex = 'm' ? 'м' : 'д';
 
-    if(maxAgeCategory <= 9)
+    if(maxAgeCategory <= 11)
         sex = sexOfCategory === 'm' ? 'мальчики' : 'девочки';
 
-    if(maxAgeCategory > 9 && maxAgeCategory <= 17)
+    if(maxAgeCategory > 11 && maxAgeCategory <= 17)
         sex = sexOfCategory === 'm' ? 'юноши' : 'девушки';
+
+    if(maxAgeCategory > 17)
+        sex = sexOfCategory === 'm' ? 'юниоры' : 'юниорки';
 
     if(weightCategoty !== 'АБС')
         weightCategoty = `${weightCategoty} кг.`;
